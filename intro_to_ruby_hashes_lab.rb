72d02badd34b73ab6dc10b7e43e7e9baf44a5e6a
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-15 15:50:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
 	monopoly = {
-  :railroads => {} , :pieces=>4, :rent_in_dollars=>
+  :railroads => {} , :pieces => 4, :rent_in_dollars =>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
